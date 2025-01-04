67936a0d72f0a89f1a7db33b1c2da8e49e9a1f0d
wiigelec <wiigelec@hotmail.com>
2025-01-04 04:08:58 +0000

    package rev

--- src/scripts/build/build-bootstrap-archives.sh
+++ src/scripts/build/build-bootstrap-archives.sh
@@ -29,5 +29,5 @@ echo "Creating archives..."
 echo
 sudo chroot $INSTALLROOT bash -e -c "LPM_PKGLOG=$LPM_PKGLOG \
 	LPM_ARCHIVE=$LPM_ARCHIVE \
-	BOOK_VERS=$BOOK_VERS \
+	BOOK_VERS=$BOOK_VERS REV=$(echo $REV | tr [:upper:] [:lower:]) \
 	/sources/work/scripts/build-archives.sh"
