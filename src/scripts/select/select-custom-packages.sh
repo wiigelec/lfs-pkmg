3fc6e943c7d4820ec5827571ec89ffbea07f8d74
wiigelec <wiigelec@hotmail.com>
2025-01-10 09:45:22 -0700

    custom tuning

--- src/scripts/select/select-custom-packages.sh
+++ src/scripts/select/select-custom-packages.sh
@@ -98,54 +98,62 @@ KCONFIG_CONFIG=$CSTMPKGS_CONFIG_OUT $MENU_CONFIG $CSTMPKGS_CONFIG_IN
 
 sed -i '/^#/d' $CSTMPKGS_CONFIG_OUT
 sed -i '/^CONFIG_MENU/d' $CSTMPKGS_CONFIG_OUT
+sed -i 's/^CONFIG_.*_//g' $CSTMPKGS_CONFIG_OUT
+sed -i 's/=y//g' $CSTMPKGS_CONFIG_OUT
+
+configout="$(cat $CSTMPKGS_CONFIG_OUT)"
 
 
-### META CONVERT GROUP AND BUILD ###
+### ITERATE CONFIG ###
 
-while IFS= read -r line;
+while : ;
 do
-	if [[ $line == "CONFIG_META_"* ]]; then
-		metafile=${line##CONFIG_META_}
-		metafile=${metafile%=y}
-		metafile=$CUSTOM_META/$metafile
-		metafile=$(cat $metafile)
-		tempfile="$metafile "
-	else
-		tempfile="$line "
-	fi
+	meta="false"
+	for co in $configout;
+	do
+		case $co in
 
-done < $CSTMPKGS_CONFIG_OUT
+			#------------------------------------------#
+			# META
+			*.meta )
+		       	meta="true"
+			metafile="$(cat $CUSTOM_META/$co)"
+			configout=${configout//$co/}
+			configout="$configout $metafile"
+				;;
+			#------------------------------------------#
 
-echo "$tempfile" | sed \
-	-e 's/\(.*.group\)/CONFIG_GROUP_\1=y/g' \
-	-e 's/\(.*.build\)/CONFIG_BUILD_\1=y/g' \
-	> $CSTMPKGS_CONFIG_OUT
+			#------------------------------------------#
+			# GROUP
+			*.group )
 
+			# write blfs pkgs list
+			cat $CUSTOM_GROUP/$co >> $BLFS_PKGS_LIST
+			configout="${configout//$co/}"
+				;;
+			#------------------------------------------#
 
-### GROUPS TO BLFS_PKGS_LIST ###
+			#------------------------------------------#
+			# BUILD
+			*.build )
+				
+			# write cstm blds list
+			echo $co >> $CSTM_BLDS_LIST
+			configout="${configout//$co/}"
+				;;
+			#------------------------------------------#
 
-[[ -f $BLFS_PKGS_LIST ]] && rm $BLFS_PKGS_LIST
-touch $BLFS_PKGS_LIST
-cfggroups=$(grep ^CONFIG_GROUP $CSTMPKGS_CONFIG_OUT || true)
-for cg in $cfggroups;
-do
-	cg=${cg##CONFIG_GROUP_}
-	cg=${cg%=y}
-	cat $CUSTOM_GROUP/$cg >> $BLFS_PKGS_LIST
+			* ) ;;
+		esac
+	done
+
+	# break
+	if [[ $meta == "false" ]]; then break; fi
 done
 
 
-### BUILDS TO CSTM_BLDS_LIST ###
+### REMOVE DUPLICATES ###
 
-[[ -f $CSTM_BLDS_LIST ]] && rm $CSTM_BLDS_LIST
-touch $CSTM_BLDS_LIST
-cfgbuilds=$(grep ^CONFIG_BUILD $CSTMPKGS_CONFIG_OUT || true)
-for cf in $cfgbuilds;
-do
-	cf=${cf##CONFIG_BUILD_}
-	cf=${cf%=y}
-	echo $cf >> $CSTM_BLDS_LIST
-done
-
-
+nodup="$(cat -n $CSTM_BLDS_LIST | sort -uk2 | sort -n | cut -f2-)"
+echo "$nodup" > $CSTM_BLDS_LIST
 
